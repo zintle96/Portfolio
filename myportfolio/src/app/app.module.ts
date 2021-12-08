edfc6eb12dce927561ad1285893f74b6a4feac1c
Academy_learner <academy_learner@Academy-learners-MacBook-Air.local>
2021-12-08 15:46:02 +0200

    made changes

--- myportfolio/src/app/app.module.ts
+++ myportfolio/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
